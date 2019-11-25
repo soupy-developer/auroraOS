e934d0dc04c45aa8bba7695a71fe8a26e23e89f0
eitan3085 <eitan3085@gmail.com>
2019-11-24 19:11:24 -0500

    write app

--- Write/app.js
+++ Write/app.js
@@ -2,3 +2,5 @@ var text = document.getElementById("writeText");
 text.contentWindow.document.designMode = "on";
 
 document.getElementById("writeBold").onmouseup = function() { text.contentWindow.document.execCommand("bold"); }
+document.getElementById("writeItalics").onmouseup = function() { text.contentWindow.document.execCommand("italics"); }
+document.getElementById("writeUnderline").onmouseup = function() { text.contentWindow.document.execCommand("underline"); }
