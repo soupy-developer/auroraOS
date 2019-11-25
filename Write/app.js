144edd4db1c301fc4b008b97cc331e300ccefb22
eitan3085 <eitan3085@gmail.com>
2019-11-24 19:29:21 -0500

    write app 2

--- Write/app.js
+++ Write/app.js
@@ -4,3 +4,11 @@ text.contentWindow.document.designMode = "on";
 document.getElementById("writeBold").onmouseup = function() { text.contentWindow.document.execCommand("bold"); }
 document.getElementById("writeItalics").onmouseup = function() { text.contentWindow.document.execCommand("italics"); }
 document.getElementById("writeUnderline").onmouseup = function() { text.contentWindow.document.execCommand("underline"); }
+
+setInterval(function() {
+  if (text.contentWindow.document.body.scrollHeight > 1000) {
+    text.style.height = text.contentWindow.document.body.scrollHeight + "px";
+  } else {
+    text.style.height = "1000px";
+  }
+}, 500)
