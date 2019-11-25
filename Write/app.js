c0e6016708064fd7086b255d7b7989594b8a3a53
eitan3085 <eitan3085@gmail.com>
2019-11-24 19:33:41 -0500

    testing

--- Write/app.js
+++ Write/app.js
@@ -2,12 +2,13 @@ var text = document.getElementById("writeText");
 text.contentWindow.document.designMode = "on";
 
 document.getElementById("writeBold").onmouseup = function() { text.contentWindow.document.execCommand("bold"); }
-document.getElementById("writeItalics").onmouseup = function() { text.contentWindow.document.execCommand("italics"); }
+document.getElementById("writeItalics").onmouseup = function() { text.contentWindow.document.execCommand("italic"); }
 document.getElementById("writeUnderline").onmouseup = function() { text.contentWindow.document.execCommand("underline"); }
 
 setInterval(function() {
   if (text.contentWindow.document.body.scrollHeight > 1000) {
     text.style.height = text.contentWindow.document.body.scrollHeight + "px";
+    alert("Write", "I_AM_NOT_A_MAC", text.contentWindow.document.body.scrollHeight + "px");
   } else {
     text.style.height = "1000px";
   }
