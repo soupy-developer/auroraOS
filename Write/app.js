3830ff7da5016bb4c75ddfe0d94e9784a1ae3c0b
eitan3085 <eitan3085@gmail.com>
2019-11-24 21:51:25 -0500

    wow write has so many features!

--- Write/app.js
+++ Write/app.js
@@ -4,6 +4,7 @@ text.contentWindow.document.designMode = "on";
 document.getElementById("writeBold").onmouseup = function() { text.contentWindow.document.execCommand("bold", false); }
 document.getElementById("writeItalics").onmouseup = function() { text.contentWindow.document.execCommand("italic", false); }
 document.getElementById("writeUnderline").onmouseup = function() { text.contentWindow.document.execCommand("underline", false); }
+document.getElementById("writeStrikethrough").onmouseup = function() { text.contentWindow.document.execCommand("strikethrough", false); }
 document.getElementById("writeFont").oninput = function() { text.contentWindow.document.execCommand("fontName", false, document.getElementById("writeFont").value); }
 document.getElementById("writeFontSize").oninput = function() { text.contentWindow.document.execCommand("fontSize", false, document.getElementById("writeFontSize").value); }
 document.getElementById("writeFormatBlock").oninput = function() { text.contentWindow.document.execCommand("removeFormat"); text.contentWindow.document.execCommand("formatBlock", false, document.getElementById("writeFormatBlock").value); }
